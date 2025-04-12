3fef8895d38b1c8668c1f85c130a11e6c6458ca0
Jagadesh8963 <jagadesh8963@gmail.com>
2025-04-12 19:35:17 +0530

    Hi Jaggu bhAAi

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+let slideIndex = 0;
+const slides = document.querySelectorAll(".slide");
+
+function moveSlide(step) {
+    slideIndex += step;
+    if (slideIndex >= slides.length) slideIndex = 0;
+    if (slideIndex < 0) slideIndex = slides.length - 1;
+    slides.forEach((slide, index) => {
+        slide.style.display = index === slideIndex ? "block" : "none";
+    });
+}
+moveSlide(0);
