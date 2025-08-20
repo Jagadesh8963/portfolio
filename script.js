de4f1e19526df530fc2f41832cfd5f2cf129a9d8
Jagadesh8963 <jagadesh8963@gmail.com>
2025-08-20 21:26:08 +0530

    update my position

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+window.addEventListener("load", function () {
+    const loader = document.getElementById("loader");
+    if (loader) {
+      loader.style.display = "none";
+    }
+  });
 // Toggle mobile menu
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
